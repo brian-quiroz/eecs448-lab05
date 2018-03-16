b8860f591fc8be386997f792d4c4f01a7e03e1a7
Brian Quiroz <r229q057@ku.edu>
2018-03-16 15:07:00 -0500

    finished exercise 3

--- exercise2/CreateUser.php
+++ exercise2/CreateUser.php
@@ -3,21 +3,25 @@ $mysqli = new mysqli("mysql.eecs.ku.edu", "r229q057", "yuF4cuNa", "r229q057");
 
 $username = $_POST["username"];
 
+$blankUsername = false;
+
 /* check connection */
 if ($mysqli->connect_errno) {
 	printf("Connect failed: %s\n", $mysqli->connect_error);
 	exit();
 }
 
-$query = "INSERT INTO Users(user_id) VALUES ('$username')";
+if($username === "") {
+	echo"Error! Your username cannot be blank.<br>";
+	$blankUsername = true;
+}
 
-if($username == "") {
-	echo"Error! The username cannot be blank.";
-} else {
+if (!$blankUsername) {
+	$query = "INSERT INTO Users(user_id) VALUES ('$username')";
 	if ($mysqli->query($query)) {
-		echo"Name successfully added!";
+			echo"Name successfully added!<br>";
 	} else {
-		echo"Error! The username you are trying to add already exists in the database.";
+			echo"Error! The username you are trying to add already exists in the database.<br>";
 	}
 }
 
